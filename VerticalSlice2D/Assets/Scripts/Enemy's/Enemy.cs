dde79edc232eb9d26454b94b3fa05c9aedc4318d
Daan Ruting <daan.j.ruting@gmail.com>
2018-01-10 11:57:42 +0100

    Main enemy class done
    Now working on a stationary enemy Script

--- VerticalSlice2D/Assets/Scripts/Enemy's/Enemy.cs
+++ VerticalSlice2D/Assets/Scripts/Enemy's/Enemy.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+
+    public float health;
+    public GameObject physicsObj;
+    public GameObject animationObj;
+    public enum State {idle, moving, attack, dead};
+
+    public Collider2D collision;
+    public Animator animator;
+    public State enemyState;
+
+
+    public virtual void Damage()
+    {
+        health--;
+    }
+
+    public virtual void Death()
+    {
+        if (health <= 0)
+        {
+            animator.SetTrigger("death");
+        }
+    }
+
+    public virtual void Start () {
+        collision = physicsObj.GetComponent<Collider2D>();
+        animator = animationObj.GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            Damage();
+        } 
+    }
+
 }
--- VerticalSlice2D/Assets/Scripts/Enemy's/Stationary/StationaryEnemy.cs
+++ VerticalSlice2D/Assets/Scripts/Enemy's/Stationary/StationaryEnemy.cs
@@ -2,15 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StationaryEnemy : MonoBehaviour {
+public class StationaryEnemy : Enemy {
 
-	// Use this for initialization
-	void Start () {
-		
+    private float radius = 5;
+    private SphereCollider _detectZone;
+    private bool _inRange = false;
+    private void EnemyRoutine()
+    {
+        switch (base.enemyState)
+        {
+            case State.attack:
+                break;
+            case State.idle:
+                break;
+            case State.dead:
+                break;
+            default:
+                Debug.Log("State is not defined. Setting it to idle");
+                break;
+        }
+    }
+    private void RoutineSwitch()
+    {
+        switch (base.enemyState)
+        {
+            case State.idle:
+                if (_inRange)
+                {
+                    base.enemyState = State.attack;
+                }
+                break;
+            case State.attack:
+                if (!_inRange)
+                {
+                    base.enemyState = State.idle;
+                }
+                break;
+            default:
+                base.enemyState = State.idle;
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            _inRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            _inRange = false;
+        }
+    }
+
+    public override void Start () {
+        base.Start();
+        _detectZone = new SphereCollider();
+        _detectZone.isTrigger = true;
+        _detectZone.radius = radius;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        base.Death();
+        EnemyRoutine();
+        RoutineSwitch();
 	}
 }
