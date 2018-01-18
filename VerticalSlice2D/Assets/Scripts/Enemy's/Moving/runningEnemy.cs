5ca4ebd2b133efa9f0558c72935516def06527c4
RemcoRemerij <r.remery@hotmail.com>
2018-01-18 13:50:06 +0100

    Revert "Merge branch 'master' of https://github.com/Krokoloko/VerticalSlice2D"
    This reverts commit f7d991a91960074291173658e15cea3d6a844276, reversing
    changes made to cb847927ffb182ec199ff5566552635972aeebaa.

--- VerticalSlice2D/Assets/Scripts/Enemy's/Moving/runningEnemy.cs
+++ VerticalSlice2D/Assets/Scripts/Enemy's/Moving/runningEnemy.cs
@@ -2,81 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunningEnemy : Enemy {
+public class runningEnemy : Enemy {
 
     private Ray ray;
     private string platform;
-    
     public bool leftSide;
-    public float walkingSpeed;
-    public Transform firstPoint;
-    public Transform secondPoint;
 
     private void CheckSide()
     {
-        if (leftSide)
+        if (transform.position.x < transform.position.x)
         {
-            if (firstPoint.position.x >= transform.position.x)
-            {
-                leftSide = false;
-            }
+            leftSide = false;
+            base.spriteRend.flipY = true;
         }
         else
         {
-            if (secondPoint.position.x <= transform.position.x)
-            {
-                leftSide = true;
-            }
+            leftSide = true;
+            base.spriteRend.flipY = false;
         }
-        base.spriteRend.flipY = leftSide;
     }
 
     public override void Start () {
-        base.Start();
-        leftSide = true; //base.RandomBool();
-        base.enemyState = State.moving;
-
-    }
-
-    private void EnemyRoutine()
-    {
-        switch (base.enemyState)
-        {
-            case State.moving:
-                if (leftSide)
-                {
-                    transform.Translate(Vector3.left * Time.deltaTime * walkingSpeed);
-                }
-                else
-                {
-                    transform.Translate(Vector3.right * Time.deltaTime * walkingSpeed);
-                }
-                break;
-            case State.dead:
-                Destroy(gameObject);
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void RoutineSwitch()
-    {
-        switch (base.enemyState)
-        {
-            case State.moving:
-                CheckSide();
-                break;
-            case State.idle:
-                base.enemyState = State.moving;
-                break;
-        }
-    }
-
-    void Update () {
-        EnemyRoutine();
-        RoutineSwitch();
-        Debug.Log(leftSide);
-        
-    }
+		
+	}
+	
+	void Update () {
+	    	
+	}
 }
