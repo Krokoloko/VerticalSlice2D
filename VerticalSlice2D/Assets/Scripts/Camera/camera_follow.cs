a62d0425f1b5aa8d16cc5b485c8f4d309aabd9df
Daan Ruting <daan.j.ruting@gmail.com>
2018-01-23 14:09:13 +0100

    parallax added
    Also with a temporary camera movement(not following the player yet)

---
+++ VerticalSlice2D/Assets/Scripts/Camera/camera_follow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_follow : MonoBehaviour {
+
+    public GameObject firstEnd;
+    public GameObject secondEnd;
+
+    private float firstX;
+    private float secondX;
+
+
+	void Start () {
+        firstX = firstEnd.transform.position.x;
+        secondX = secondEnd.transform.position.x;
+	}
+
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, firstX, secondX),transform.position.y,transform.position.z);    
+    }
+
+    void LateUpdate () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left/5);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right/5);
+        }
+	}
+}
