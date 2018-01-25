4a5edfc3f36b94b7dd4723a05e056b37dbcd0446
Daan Ruting <daan.j.ruting@gmail.com>
2018-01-25 09:47:10 +0100

    Making new enemy script
    Going to make another script inherited from the enemy class

---
+++ VerticalSlice2D/Assets/Scripts/Enemy's/Moving/PlantEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantEnemy : Enemy {
+
+    private BoxCollider2D _detectZone;
+
+    public override void Start () {
+        base.Start();
+	}
+
+    private void RoutineSwitch()
+    {
+        switch (base.enemyState)
+        {
+            case State.idle:
+                if ()
+                {
+
+                }
+                break;
+            case State.attack:
+                break;
+            case State.moving:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void EnemyRoutine()
+    {
+        switch (base.enemyState)
+        {
+            case State.idle:
+                break;
+            case State.attack:
+                break;
+            case State.moving:
+                break;
+            default:
+                break;
+        }
+    }
+	void Update () {
+		
+	}
+}
